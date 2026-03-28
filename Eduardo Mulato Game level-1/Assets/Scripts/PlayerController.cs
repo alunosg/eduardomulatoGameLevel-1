f4bbea557f942c26fc055c4e17f68b34875a1483
alunosg <alunosg.altodalapa@gmail.com>
2026-03-28 12:26:50 -0300

    animate
    animate

--- Eduardo Mulato Game level-1/Assets/Scripts/PlayerController.cs
+++ Eduardo Mulato Game level-1/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10;
     public float jumpForce = 10;
     public LayerMask floorLayer;
+    public Animator anim;
+    private bool grounded = false;
 
     private Vector2  moveInput;
 
@@ -16,29 +18,48 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        anim.SetBool("IsMoving", moveInput.x != 0);
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(context.started)
-        {
-            Vector2 leftPoint = new(col.bounds.min.x, col.bounds.min.y);
-            Vector2 rightPoint = new(col.bounds.max.x, col.bounds.max.y);
+        if(context.started && grounded)
 
-            if(Physics2D.Raycast(leftPoint, Vector2.down,  col.bounds.size.y * 1.1f, floorLayer) ||
-                Physics2D.Raycast(rightPoint, Vector2.down, col.bounds.size.y *1.1f, floorLayer))
-            {
-                rig.linearVelocity = new(rig.linearVelocity.x, jumpForce);
-            }
+        {
+            anim.SetTrigger("Jump");
+            rig.linearVelocity = new(rig.linearVelocity.x, jumpForce);
         }
 
     }
+
+    private void GroundedCheck()
+    {
+        Vector2 leftPoint = new(col.bounds.min.x, col.bounds.min.y);
+        Vector2 rightPoint = new(col.bounds.max.x, col.bounds.max.y);
+
+        if (Physics2D.Raycast(leftPoint, Vector2.down, col.bounds.size.y * 1.1f, floorLayer) ||
+            Physics2D.Raycast(rightPoint, Vector2.down, col.bounds.size.y * 1.1f, floorLayer))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        anim.SetBool("IsGrounded", grounded);
+
+    }
+
+
+            
     public void FixedUpdate()
     {
+        GroundedCheck();
         rig.linearVelocity = new Vector2(moveInput.x * speed, rig.linearVelocity.y);
 
     }
-}
+}   
 
 
 
